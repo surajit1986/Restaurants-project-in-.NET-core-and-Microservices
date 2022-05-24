eea6f9f669297537a195aeb3832d3126b2e0fa18
surajit1986 <surajit.1986@gmail.com>
2022-05-24 22:29:46 +0530

    folder structure and solution files are checked in

---
+++ Demo Restaurant/Restaurant.Services.Product/DBContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant.Services.Product.DBContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+
+        }
+    }
+}
